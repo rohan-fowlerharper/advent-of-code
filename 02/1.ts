5e77af405e01c427dfbbc703615802cc3b66e831
rohan-fowlerharper <rohan.fowlerharper@gmail.com>
2022-12-02 18:31:31 +1300

    solutions day 2

--- 02/1.ts
+++ 02/1.ts
@@ -5,3 +5,40 @@ const input = await Deno.readTextFile(
 )
 
 const lines = input.split('\n')
+
+let points = 0
+for (const line of lines) {
+  const [opp, me] = line.split(' ')
+
+  let round = 0
+  switch (me) {
+    case 'X':
+      round += 1
+      if (opp === 'A') {
+        round += 3
+      } else if (opp === 'C') {
+        round += 6
+      }
+      break
+    case 'Y':
+      round += 2
+      if (opp === 'A') {
+        round += 6
+      } else if (opp === 'B') {
+        round += 3
+      }
+      break
+    case 'Z':
+      round += 3
+      if (opp === 'B') {
+        round += 6
+      } else if (opp === 'C') {
+        round += 3
+      }
+      break
+  }
+
+  points += round
+}
+
+console.log(points)
--- 02/2.ts
+++ 02/2.ts
@@ -5,3 +5,46 @@ const input = await Deno.readTextFile(
 )
 
 const lines = input.split('\n')
+
+let points = 0
+for (const line of lines) {
+  const [opp, me] = line.split(' ')
+
+  let round = 0
+  switch (me) {
+    case 'X':
+      round += 0
+      if (opp === 'A') {
+        round += 3
+      } else if (opp === 'B') {
+        round += 1
+      } else {
+        round += 2
+      }
+      break
+    case 'Y':
+      round += 3
+      if (opp === 'A') {
+        round += 1
+      } else if (opp === 'B') {
+        round += 2
+      } else {
+        round += 3
+      }
+      break
+    case 'Z':
+      round += 6
+      if (opp === 'A') {
+        round += 2
+      } else if (opp === 'B') {
+        round += 3
+      } else {
+        round += 1
+      }
+      break
+  }
+  console.log(opp, me, round)
+  points += round
+}
+
+console.log(points)
